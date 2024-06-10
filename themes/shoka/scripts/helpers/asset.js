855dbe9b0c41643c6420d36fe7e65865bc43f41f
zhangtuo <zhangtuo@zhangtuodeMacBook-Pro.local>
2024-06-10 15:41:43 +0800

    fix: 取消特殊字体

--- themes/shoka/scripts/helpers/asset.js
+++ themes/shoka/scripts/helpers/asset.js
@@ -13,6 +13,7 @@ hexo.extend.helper.register('theme_env', function (type) {
 })
 
 hexo.extend.helper.register('_vendor_font', () => {
+  return
   const config = hexo.theme.config.font;
 
   if (!config || !config.enable) return '';
@@ -21,6 +22,7 @@ hexo.extend.helper.register('_vendor_font', () => {
   const fontSubset = '&subset=latin,latin-ext';
   const fontStyles = ':300,300italic,400,400italic,700,700italic';
   const fontHost = '//fonts.googleapis.com';
+  // const fontHost = '//fonts.proxy.ustclug.org'
 
   //Get a font list from config
   let fontFamilies = ['global', 'logo', 'title', 'headings', 'posts', 'codes'].map(item => {
